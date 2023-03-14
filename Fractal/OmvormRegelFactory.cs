ad890cfaa8ee98ffd0da8bd3ec2e6f422eddcfbe
simonbaeke <baeke_S@hotmail.com>
2023-03-14 09:20:25 +0100

    aanpassen loop

---
+++ Fractal/OmvormRegelFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Fractal
+{
+	public class OmvormRegelFactory
+	{
+		public OmvormRegelFactory()
+		{
+
+		}
+
+		public static Dictionary<string,string> Parse(string[] lines)
+		{
+            Dictionary<string, string> regels = new Dictionary<string, string>();
+
+            foreach (string line in lines)
+            {
+				string regel = line.Split("=>")[0].Trim();
+				string uitbreiding = line.Split("=>")[1].Trim();
+
+                regels.Add(line.Split("=>")[0].Trim(), line.Split("=>")[1].Trim());
+
+				//TODO spiegelen en draaien
+            }
+
+            return regels;
+		}
+
+		private string Roteer(string regel)
+		{
+
+
+			return regel;
+		}
+
+		private string Spiegel(string regel)
+		{
+			return regel;
+		}
+
+
+	}
+}
+
